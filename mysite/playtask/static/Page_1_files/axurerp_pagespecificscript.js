27e0dd73974364d0671847b143add0bb0f574c36
google2050 <appleface2050@qq.com>
2016-01-18 15:25:38 +0800

    add static file

---
+++ mysite/playtask/static/Page_1_files/axurerp_pagespecificscript.js
@@ -0,0 +1,7 @@
+﻿for(var i = 0; i < 55; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u17'] = 'top';gv_vAlignTable['u51'] = 'top';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u15'] = 'top';gv_vAlignTable['u1'] = 'top';gv_vAlignTable['u39'] = 'top';gv_vAlignTable['u11'] = 'top';gv_vAlignTable['u38'] = 'center';gv_vAlignTable['u9'] = 'top';gv_vAlignTable['u7'] = 'top';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u24'] = 'center';gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u54'] = 'top';gv_vAlignTable['u19'] = 'top';gv_vAlignTable['u5'] = 'top';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u50'] = 'center';gv_vAlignTable['u53'] = 'center';gv_vAlignTable['u0'] = 'top';
--- mysite/playtask/views.py
+++ mysite/playtask/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
+from .models import Task
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the polls index.")
+#
 
 def index(request):
+    latest_task_list = Task.objects.order_by('-update')[:5]
+    # template = loader.get_template('index_test.html')
+    # output = ', '.join([q.title for q in latest_task_list])
+    # print output
+    # return HttpResponse(output)
+
+    context = {"latest_task_list":latest_task_list}
+
+    # return HttpResponse(template.render(context, request))
+    return render(request, "index.html", context)
+
+
+
+def test(request):
     return HttpResponse("Hello, world. You're at the polls index.")
